f69456f3294387057793d54cbd129fa5e56b7654
Leander Berg <reg@lulebe.net>
2021-05-01 07:00:03 +0200

    major rework

--- brunch-config.js
+++ brunch-config.js
@@ -1,7 +1,7 @@
 exports.config = {
   paths: {
-    public: './static',
-    watched: ['scss']
+    public: './',
+    watched: ['scss', 'templates']
   },
   files: {
     stylesheets: {
@@ -14,5 +14,43 @@ exports.config = {
         includePaths: ['scss']
       }
     }
+  },
+  hooks: {
+    onCompile: compileTemplates
   }
-};
+}
+
+const fs = require('fs')
+const path = require('path')
+const htmlmin = require('html-minifier').minify
+
+function compileTemplates () {
+  const startTime = (new Date()).getTime()
+  const templates = compileTemplatesInFolder('./templates')
+  fs.writeFileSync('./templates.json', JSON.stringify(templates))
+  const duration = (new Date()).getTime() - startTime
+  const d = new Date()
+  const time = (""+d.getHours()).padStart(2, "0") + ":" + (""+d.getMinutes()).padStart(2, "0") + ":" + (""+d.getSeconds()).padStart(2, "0")
+  console.info(time + ' - \x1b[32minfo\x1b[0m: %s', "compiled templates in " + duration + "ms.")
+}
+
+function compileTemplatesInFolder (dirPath) {
+  const templates = {card: {}}
+  const listing = fs.readdirSync(dirPath)
+  listing
+        .filter(item => fs.lstatSync(path.join(dirPath, item)).isFile())
+        .forEach(item => {
+          const html = htmlmin(fs.readFileSync(path.join(dirPath, item), 'utf-8'), {
+            removeAttributeQuotes: true,
+            removeComments: true,
+            removeAttributeQuotes: true,
+            removeTagWhitespace: true
+          })
+          templates.card[item.split('.')[0]] = html
+        })
+  templates.data = listing
+                          .filter(item => fs.lstatSync(path.join(dirPath, item)).isDirectory())
+                          .sort((a, b) => parseInt(path.basename(a)) - parseInt(path.basename(b)))
+                          .map(item => compileTemplatesInFolder(path.join(dirPath, item)))
+  return templates
+}
---
+++ js/main.js
@@ -0,0 +1,89 @@
+let language = 'de'
+let currentPath = []
+let data = {}
+
+const cardsWrapper = document.querySelector('.cards-wrapper')
+
+function setBackBtnVisibility () {
+  const backBtn = document.querySelector('.back-btn')
+  if (currentPath.length)
+    backBtn.classList.add('visible')
+  else
+    backBtn.classList.remove('visible')
+}
+
+function back () {
+  if (!currentPath.length) return
+  currentPath.pop()
+  const content = renderCard(currentPath)
+  const oldCard = cardsWrapper.lastElementChild
+  const newCard = document.createElement('div')
+  newCard.classList.add('card', 'offscreen')
+  newCard.innerHTML = content
+  cardsWrapper.appendChild(newCard)
+  setTimeout(() => {
+    newCard.classList.remove('offscreen')
+  }, 30)
+  setTimeout(() => {
+    cardsWrapper.removeChild(oldCard)
+  }, 500)
+  setBackBtnVisibility()
+}
+
+function showNextCard (content) {
+  const oldCard = cardsWrapper.lastElementChild
+  const newCard = document.createElement('div')
+  newCard.classList.add('card')
+  newCard.innerHTML = content
+  cardsWrapper.insertBefore(newCard, oldCard)
+  oldCard.classList.add('offscreen')
+  setTimeout(() => {
+    cardsWrapper.removeChild(oldCard)
+  }, 500)
+  setBackBtnVisibility()
+}
+
+function renderCard (path) {
+  let currentData = data
+  path.forEach(item => {
+    currentData = currentData.data[item]
+  })
+  return currentData.card[language] || currentData.card['en']
+}
+
+function toggleLanguageMenu() {
+  document.querySelector('.language-menu').classList.toggle('visible')
+}
+
+function setLanguage (lang) {
+  language = lang
+  document.querySelector('.language-menu').classList.remove('visible')
+  const content = renderCard(currentPath)
+  const oldCard = cardsWrapper.lastElementChild
+  const newCard = document.createElement('div')
+  newCard.classList.add('card', 'newlang')
+  newCard.innerHTML = content
+  cardsWrapper.insertBefore(newCard, oldCard)
+  setTimeout(() => {
+    oldCard.classList.add('oldlang')
+    newCard.classList.remove('newlang')
+  }, 30)
+  setTimeout(() => {
+    cardsWrapper.removeChild(oldCard)
+  }, 500)
+}
+
+function action (num) {
+  currentPath.push(num)
+  showNextCard(renderCard(currentPath))
+}
+
+(function () {
+  qwest.get('/templates.json', {}, {responseType: 'json'})
+  .then((_, res) => {
+    data = res
+  })
+  .then(() => {
+    showNextCard(renderCard([]))
+  })
+})()
